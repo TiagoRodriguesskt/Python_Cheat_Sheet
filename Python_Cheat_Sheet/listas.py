168b00b152dfb0de263cfaed2b96bc5af88851ba
TiagoRodriguesskt <tiagorodriguesskt@gmail.com>
2026-03-26 18:18:30 -0300

    atualização

---
+++ Python_Cheat_Sheet/listas.py
@@ -0,0 +1,17 @@
+"""
+DocStrings: --> A list stores a series of items in a particular order.
+You access items using an index, or within a loop.
+"""
+
+"""Make a list"""
+bikes = ["trek", "redline", "giant"]
+
+"""Get the first item in a list"""
+
+"""Get the last item in a list"""
+
+"""Looping througn a list"""
+
+"""Adding items to a list"""
+
+"""Making numerical list"""
